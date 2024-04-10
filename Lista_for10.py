3cd118838764249de1728006f2116eae3eda6ea9
leograttao <162933108+leograttao@users.noreply.github.com>
2024-04-10 18:58:43 -0300

    Add files via upload

--- Lista_for10.py
+++ Lista_for10.py
@@ -1,9 +1,9 @@
 lista = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
 repetido = 0
-maior = 0
-for c in range(0, 11):
-    vezes = lista.count(c)
-    if vezes > maior:
-        maior = vezes
-        repetido = c
+maior_repeticao = 0
+for i in range(0, 11):
+    repeticao = lista.count(i)
+    if repeticao > maior_repeticao:
+        maior_repeticao = repeticao
+        repetido = i
 print(repetido)
